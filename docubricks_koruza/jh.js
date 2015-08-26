08b3bca6ead02661d550ff8aa3bbb31746ff90ca
Johan Henriksson <mahogny@areta.org>
2015-08-26 14:28:54 +0200

    xsl now works standalone

--- docubricks_koruza/jh.js
+++ docubricks_koruza/jh.js
@@ -8,8 +8,7 @@
  * @param m			Brick map
  * 
  */
-function renderBricksTreeR(db,lev,elemTree, m){
-	
+function renderBricksTreeR(db,lev,elemTree, m){	
 	for(var i=0;i<lev.length;i++){
 		
 		var etop = document.createElement("li");
@@ -148,9 +147,7 @@ function getBricksTreeR(m,parentid){
 
 
 /**
- * 
  * Function to return count for physical item
- * 
  */
 function getPhysicalPartCount(db){
 	//Set all to 0
@@ -231,11 +228,414 @@ function loadxml(){
 	// handle response
 	function XHRhandler() {
 		if (xhr.readyState == 4) {
+			console.log(xhr.responseXML.documentElement);
 			var obj = XML2jsobj(xhr.responseXML.documentElement);
-			//console.log(obj);
+			console.log(obj);
 			xhr = null;
 			headx(obj);
 		}
+	}
+
+}
+
+
+
+function getXmlString($xmlObj){   
+    var xmlString="";
+    $xmlObj.children().each(function(){
+        xmlString+="<"+this.nodeName+">";
+        if($(this).children().length>0){
+            xmlString+=getXmlString($(this));
+        }
+        else
+            xmlString+=$(this).text();
+        xmlString+="</"+this.nodeName+">";
+    });
+    return xmlString;
+}
+
+
+function string2xml(txt){
+	if (window.DOMParser)
+	  {
+	  parser=new DOMParser();
+	  return parser.parseFromString(txt,"text/xml");
+	  }
+	else // Internet Explorer
+	  {
+	  xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+	  xmlDoc.async=false;
+	  xmlDoc.loadXML(txt);
+	  return xmlDoc;
+	  } 
+}
+
+function loadxml2(){
+	//TODO
+	/*
+	var xmls = getXmlString($("project"));
+	alert($("project").get());
+	alert(XML2jsobj(xmls));
+	*/
+	xmls = document.getElementById("hiddendata").children[0];
+	xmls = new XMLSerializer().serializeToString(xmls);
+	xmls = string2xml(xmls).documentElement;
+	
+	console.log(xmls);
+	
+	headx(XML2jsobj(xmls));
+}
+
+
+
+/**
+ * 
+ * @param db
+ */
+function headx(db){
+	
+	//a id="pname" class="name" href="#"><span></span></a>	
+//	 alert(db);
+	 
+	renderBricksTree(db);
+	
+	
+	/////////////////////////////////////
+	var q1=document.createElement("a");
+	q1.setAttribute("class", "name");
+	q1.setAttribute("href","#");
+	var qn=document.createTextNode(db.project_name); ///////////// TODO: use top-level brick name
+	q1.appendChild(qn);
+	qx=document.getElementById('name-box');
+	qx.appendChild(q1);
+
+	
+	for (var i=0; i < db.unit.length; i++) {
+		var qj=document.createElement("a");
+		qj.setAttribute("class", "button");
+		qj.setAttribute("href","#");
+		qj.setAttribute("style", "text-align: right; font-size: .35rem; font-weight: 400; line-height: 34px; letter-spacing= .1rem; padding: 0 9px;");
+		var qn=document.createTextNode("brk"+i.toString());
+		qj.appendChild(qn);
+		qy=document.getElementById("cola");
+		qy.appendChild(qj);
+	}
+
+
+	////////////////////////////////////////////////////////////////////////////////
+	var dx=document.getElementById("ccentre");
+	var spaces=document.createElement("br");
+	dx.appendChild(spaces);
+	var spaces=document.createElement("br");
+	dx.appendChild(spaces);
+
+	///////////////////////////////////////////////////////////////////////////////
+	/// All the bricks
+	for (var i=0; i < db.unit.length; i++) {
+		var thisunit=db.unit[i];
+		addBrick(dx, thisunit);
+	}
+		
+}
+
+/**
+ * Return a string as "" if undefined
+ */
+function text0(t){
+	if(t!=undefined && t.length!=undefined)
+		return t;
+	else
+		return "";
+} 
+
+/**
+ * Add one brick
+ */
+function addBrick(dx, thisunit){
+	var nm = thisunit.name;
+
+	var qj1=document.createElement("div");
+	qj1.setAttribute("class","row");
+
+	var qj1a=document.createElement("div");
+	qj1a.setAttribute("class","col7 colExample");
+
+	var pqja=document.createElement("p");
+	pqja.setAttribute("align","left");
+
+	var h1a=document.createElement("h1");
+	var t1a=document.createTextNode("Brick: "/*+(i+1).toString()+":"*/+thisunit.name);
+					//TODO should rather check what is the top-level brick! best to separate out the code to place one component into a separate method
+											
+	h1a.appendChild(t1a);
+	pqja.appendChild(h1a);
+	qj1a.appendChild(pqja);
+	qj1.appendChild(qj1a);
+	dx.appendChild(qj1);
+
+	thisunit.abstract=text0(thisunit["abstract"]);
+	if(thisunit.abstract!=""){
+		var pqjb=document.createElement("p");
+		pqjb.setAttribute("align","left");
+		var text=document.createTextNode(thisunit.abstract);
+		pqjb.appendChild(text);
+		qj1a.appendChild(pqjb);
+		
+	}
+
+	var toi=typeof thisunit.media[0];
+	var qj1b=document.createElement("div");
+	qj1b.setAttribute("class","col5 colExample");
+	var img=document.createElement("img");
+	if(toi == "undefined"){
+						//dynamically add an image and set its attribute
+		img.setAttribute("src",'images/Logo.png');
+							//img.id="picture"
+	}else{
+		img.setAttribute("src",thisunit.media[0]);
+						//img.id="picture"
+	}
+	qj1b.appendChild(img);
+	qj1.appendChild(qj1b);
+	dx.appendChild(qj1);
+
+	var wwhow=document.createElement("div");
+	wwhow.setAttribute("class","row");
+
+	////////////////////////////////////////////////////////////////////////
+
+	addsomehow(dx, thisunit, "what", "What: ");
+	addsomehow(dx, thisunit, "why", "Why: ");
+	addsomehow(dx, thisunit, "how", "How: ");
+
+	///////////////////////////////////////////////////////////////////////////
+	// Legal
+	
+	var anyLegal=false;
+	
+	var legalnode=document.createElement("div")
+	legalnode.setAttribute("class","col12 colExample");
+
+	var qlegnodea=document.createElement("p");
+	qlegnodea.setAttribute("align","left");
+	var qlegnodeb=document.createElement("h1");
+	var qlegnodec=document.createTextNode("Legal.");
+	qlegnodeb.appendChild(qlegnodec);
+	qlegnodea.appendChild(qlegnodeb);
+	legalnode.appendChild(qlegnodea);
+
+
+	var plegnodeax=document.createElement("p");
+	plegnodeax.setAttribute("align","left");
+
+	thisunit.license=text0(thisunit["license"]);
+	if(thisunit.license!=""){
+		var txta=document.createTextNode("Licence:");
+		plegnodeax.appendChild(txta);
+		legalnode.appendChild(plegnodeax);
+		
+		var txta2=document.createTextNode(thisunit.license);
+		plegnodex.appendChild(txta2);
+		legalnode.appendChild(plegnodex);
+		
+		anyLegal=true;
+	}
+
+
+	if(true){
+		var plegnodebx=document.createElement("p");
+		plegnodebx.setAttribute("align","left");
+		var txtb=document.createTextNode("Authors:");
+		plegnodebx.appendChild(txtb);
+		legalnode.appendChild(plegnodebx);
+	}
+
+
+	if(true){
+		var plegnodecx=document.createElement("p");
+		plegnodecx.setAttribute("align","left");
+		var txtc=document.createTextNode("Copyright:");
+		plegnodecx.appendChild(txtc);
+		legalnode.appendChild(plegnodecx);
+	}
+
+	if(anyLegal)
+		dx.appendChild(legalnode);
+	
+	//////////////////////////////////////////////////////////////////////////
+	var insnode=document.createElement("div");
+	insnode.setAttribute("class","row");
+
+	var insa=document.createElement("div");
+	insa.setAttribute("class","col4 colExample");
+	var lka=document.createElement("a");
+	lka.setAttribute("href","#");
+	var htxlka=document.createElement("h4");
+	var txlka=document.createTextNode("Assembly Instructions");
+	htxlka.appendChild(txlka);
+	lka.appendChild(htxlka);
+	insa.appendChild(lka);
+
+	insnode.appendChild(insa);
+
+	var insb=document.createElement("div");
+	insb.setAttribute("class","col4 colExample");
+	var lkb=document.createElement("a");
+	lkb.setAttribute("href","#");
+	var htxlkb=document.createElement("h4");
+	var txlkb=document.createTextNode("Test Instructions");
+	htxlkb.appendChild(txlkb);
+	lkb.appendChild(htxlkb);
+	insb.appendChild(lkb);
+
+	insnode.appendChild(insb);
+
+	var insc=document.createElement("div");
+	insc.setAttribute("class","col4 colExample");
+	var lkc=document.createElement("a");
+	lkc.setAttribute("href","#");
+	var htxlkc=document.createElement("h4");
+	var txlkc=document.createTextNode("BOM");
+	htxlkc.appendChild(txlkc);
+	lkc.appendChild(htxlkc);
+	insc.appendChild(lkc);
+
+	insnode.appendChild(insc);
+	dx.appendChild(insnode);
+	//////////////////////////////////////////////////////////////////////////
+
+
+	if(!("assembly_instruction" in thisunit))
+		thisunit.assembly_instruction={};
+	addInstruction(dx, thisunit, thisunit.assembly_instruction);
+	 
+}
+
+
+/**
+ * Add one set of instructions
+ */
+function addInstruction(dx, thisunit, instruction){
+	
+	instruction.step = atleast1(instruction["step"]);
+
+	var assins=document.createElement("div");
+	var aihead=document.createElement("div");
+	aihead.setAttribute("class","col12 colExample");
+	var aititle=document.createElement("p");
+	aititle.setAttribute("align","left");
+	var aih1=document.createElement("h1");
+	var aihtxt=document.createTextNode(thisunit.name+ " Assembly Instructions.");
+	aih1.appendChild(aihtxt);
+	aititle.appendChild(aih1);
+	aihead.appendChild(aititle);
+	assins.appendChild(aihead);
+
+
+	if (instruction.step.length==0){
+		/*
+		var nadadiv=document.createElement("div");
+		nadadiv.setAttribute("class","col12 colExample");
+		var nadap=document.createElement("p");
+		nadap.setAttribute("align","left");
+		var nadadivtxt=document.createTextNode("No information available");
+		nadap.appendChild(nadadivtxt);
+		nadadiv.appendChild(nadap);
+		nadadiv.appendChild(nadap);
+		assins.appendChild(nadadiv);
+
+		dx.appendChild(assins);
+		*/
+	} else{
+			
+		for(var muj=0;muj<instruction.step.length;muj++){
+			thisstep=instruction.step[muj];
+			var aidivn=document.createElement("div");
+
+			var aititle=document.createElement("p");
+			aititle.setAttribute("align","left");
+			var aih3=document.createElement("h3");
+			var saihtxt=document.createTextNode("Step "+(1+muj).toString());
+
+			aih3.appendChild(saihtxt);
+			aititle.appendChild(aih3);
+			aidivn.appendChild(aititle);
+
+			var aininfo=document.createElement("div");
+			aininfo.setAttribute("class","row");
+
+			var stepnimg=document.createElement("div");
+			stepnimg.setAttribute("class","col6 colExample");
+
+			///////////////////////////////////////////////////
+			// NOOOOOOTE: there can be more than one media file!
+			var stimgsrc;
+			if("media" in thisstep && "file" in thisstep.media && "url" in thisstep.media.file){
+				stimgsrc=thisstep.media.file.url;
+				var img=document.createElement("img");
+				img.setAttribute("src",stimgsrc);
+				img.setAttribute("width","100%");
+			} else {
+				var img=document.createElement("img");
+				//dynamically add an image and set its attribute
+					img.setAttribute("src",'images/Logo.png');
+					//img.id="picture"
+				
+			}
+
+			var stepimgp=document.createElement("p")
+			stepimgp.setAttribute("align","left");
+			stepimgp.appendChild(img);
+			stepnimg.appendChild(stepimgp);
+			aininfo.appendChild(stepnimg);
+			//qj1.appendChild(stepnimg);
+			//dx.appendChild();
+
+			/////////////////////////////////////////////////////
+
+			var stepndesc=document.createElement("div");
+			stepndesc.setAttribute("class","col6 colExample");
+			var aidescp=document.createElement("p");
+			aidescp.setAttribute("align","left");
+			var aidescptxt=document.createTextNode(thisunit.assembly_instruction.step[muj].description);
+			aidescp.appendChild(aidescptxt);
+			stepndesc.appendChild(aidescp);
+			aininfo.appendChild(stepndesc);
+
+			aidivn.appendChild(aininfo);
+			assins.appendChild(aidivn);
+			dx.appendChild(assins);
+		}
+	}
+	
+}
+ 
+ 
+/**
+ * Optionally add: Why, How, What
+ */
+function addsomehow(dx, thisunit, elem, head){
+
+	var qhow=document.createElement("div");
+	if(elem in thisunit && thisunit[elem].length!=undefined){
+		qhow.setAttribute("class","col4 colExample");
+		var qhowa=document.createElement("p");
+		qhowa.setAttribute("align","left");
+		var qhowb=document.createElement("h1");
+		var qhowc=document.createTextNode(head);
+		qhowb.appendChild(qhowc);
+		qhowa.appendChild(qhowb);
+		qhow.appendChild(qhowa);
+		
+		var phow=document.createElement("p");
+		phow.setAttribute("align","left");
+		var text=document.createTextNode(thisunit[elem]);
+		phow.appendChild(text);
+		qhow.appendChild(phow);
+		dx.appendChild(qhow);
+	}
+	else{
+			dx.appendChild(qhow);
 	}
 
 }
